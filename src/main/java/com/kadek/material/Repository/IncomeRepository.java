edb40ae054f95687224fa28acb243dc0ce48c0e9
KadekAgusSaputra <149917061+KadekAgusSaputra@users.noreply.github.com>
2026-04-24 02:01:38 +0800

    fixing get dashgboard transaction

--- src/main/java/com/kadek/material/Repository/IncomeRepository.java
+++ src/main/java/com/kadek/material/Repository/IncomeRepository.java
@@ -14,21 +14,21 @@ import java.util.Optional;
 public interface IncomeRepository extends JpaRepository<Income,Long> {
 
 
-    //PAKAI NATIVE KARENA TERLALU RIBET UNTUK PAKAI JPQL DAN KEMUNGKINAN ERROR
     @Query(value = "SELECT v.id, v.truck_name, v.category, " +
-            "COALESCE(SUM(i.total_price), 0) as total_income, " +
-            "COALESCE((SELECT SUM(e.amount) FROM expenses e JOIN income inc ON e.income_id = inc.id WHERE inc.vehicle_id = v.id), 0) as total_expense, " +
-            "COALESCE(SUM(i.total_price), 0) - COALESCE((SELECT SUM(e.amount) FROM expenses e JOIN income inc ON e.income_id = inc.id WHERE inc.vehicle_id = v.id), 0) as net " +
+            "COALESCE(SUM(i.sell_price * i.quantity), 0) as total_income, " +
+            "COALESCE(SUM(i.buy_price * i.quantity), 0) + " +
+            "  COALESCE((SELECT SUM(e.amount) FROM expenses e JOIN income inc ON e.income_id = inc.id WHERE inc.vehicle_id = v.id), 0) as total_expense, " +
+            "COALESCE(SUM((i.sell_price - i.buy_price) * i.quantity), 0) - " +
+            "  COALESCE((SELECT SUM(e.amount) FROM expenses e JOIN income inc ON e.income_id = inc.id WHERE inc.vehicle_id = v.id), 0) as net " +
             "FROM vehicles v LEFT JOIN income i ON i.vehicle_id = v.id " +
             "GROUP BY v.id, v.truck_name, v.category",
             nativeQuery = true)
     List<DashboardDTO> getDashboardTransactionPerVehicle();
 
-    //PAKAI JPQL KARENA GAK TERLALU RIBET
     @Query("SELECT new com.kadek.material.Dto.LaporanDTO(" +
-            "SUM(i.totalPrice), " +
-            "(SELECT COALESCE(SUM(e.amount), 0) FROM Expenses e), " +
-            "(SUM(i.totalPrice) - (SELECT COALESCE(SUM(e.amount), 0) FROM Expenses e))) " +
+            "SUM(i.sellPrice * i.quantity), " +
+            "SUM(i.buyPrice * i.quantity) + (SELECT COALESCE(SUM(e.amount), 0) FROM Expenses e), " +
+            "SUM((i.sellPrice - i.buyPrice) * i.quantity) - (SELECT COALESCE(SUM(e.amount), 0) FROM Expenses e)) " +
             "FROM Income i")
     LaporanDTO getGrandTotalReport();
 
